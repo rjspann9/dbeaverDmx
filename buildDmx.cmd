7dab4fc46cd4c8db74694a4627a6d8fe67a6b410
Robert Spann <spann@panopli.net>
2024-08-25 12:11:59 -0600

    An initial working version of DataMagiX
    This is a simple proof of concept of adding functionality to DBeaver.
    
    (After selecting the "Test Hello" menu item from the DataMagiX menu,
    	a simple java module is written to C:\DmxDBeaverTest folder.)
    
    Author:    Robert Spann <spann@panopli.net>
    
     On branch DataMagiX240820
     Changes to be committed:
    	modified:   build.cmd
    	modified:   features/org.jkiss.dbeaver.standalone.feature/pom.xml
    	modified:   features/pom.xml
    	modified:   plugins-dev/org.jkiss.dbeaver.ext.test/plugin.xml
    	modified:   plugins/pom.xml

---
+++ buildDmx.cmd
@@ -0,0 +1,6 @@
+set MAVEN_OPTS=-Xmx2048m
+IF NOT EXIST ..\dbeaver-common git clone https://github.com/dbeaver/dbeaver-common.git ..\dbeaver-common
+cd product/aggregate
+call mvn clean package -Pall-platforms -T 1C %1
+cd ../..
+REM pause
